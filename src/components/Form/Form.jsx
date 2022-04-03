4742461bb8c74752ba2d4c3694f109e9efbfe9f7
nasim-ahmed <nasim.ahmed54@gmail.com>
2022-04-03 12:42:04 +0200

    form submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,21 +1,35 @@
+import { useState } from 'react';
 import styles from './Form.module.css'
 
 const Form = () => {
+    const [location, setLocation] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (!location || location === '') return;
+        alert(location)
+    }
+
+    function handleChange(event){ 
+        setLocation(event.target.value)
+    }
+
     return ( 
-        <form>
+        <form onSubmit={onSubmit}>
             <input
             type="text"
             name="city"
+            required
             placeholder="Please type city" 
             autocomplete="off"
+            value = {location}
             className={`form-control ${styles.input} my-3`}
+            onChange= {handleChange}
             />
 
-            {/* <button
-                type="button"
-                className="btn btn-success">
+            <button type="button" className={`btn btn-success ${styles.search_button} my-2`} onClick={onSubmit}>
                     Search
-            </button> */}
+            </button>
         </form>   
      );
 }
